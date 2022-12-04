bab39f50baab532c3d2008369ab4fcc0127bdcbb
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-12-04 18:59:16 +0900

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash -s -- -v v1.20.0
+
+# single quote
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash -s -- -v 'v1.20.0'
+
+# double quote
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash -s -- -v "v1.20.0"
+
+# run aqua-installer without pipe
+curl -O -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer
+chmod a+x aqua-installer
+./aqua-installer -v v1.20.0
+
+# use aqua-installer -i option
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash -s -- -v v1.20.0 -i bin/aqua-installer
+
+# use aqua-installer -i option
+curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.1.2/aqua-installer | bash -s -- -i bin/aqua-installer -v v1.20.0
